db09a5c7f8fe56b174fae3b7ac8d8cb713950985
Anup Nagarkoti <anupnagarkoti@Anups-MacBook-Pro.local>
2019-04-12 23:08:42 +0545

    Initial commit

---
+++ app/src/main/java/com/e/typeandrunanup/CarAnup.java
@@ -0,0 +1,23 @@
+package com.e.typeandrunanup;
+
+public class CarAnup {
+    private String make, year, color, price, engine, Answer;
+    private int count;
+    public CarAnup(String make, String year, String color, String price, String engine, int count) {
+        this.make = make;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+        this.engine = engine;
+        this.count = count;
+    }
+
+    public String CreatesCar(){
+
+
+        Answer= "The is Vehicle No. "+count+"\n"+" Manufacturer:" +
+                " "+make+" Current Value: "+price+ " Effective engine Size: "+engine+"\n";
+        return Answer;
+
+    }
+}
